206217291a972d0c65283cc05ad3305618fec8c8
Gabriele <Gabriele@MacBook-Pro-5.fritz.box>
2024-08-26 15:11:01 +0200

    a

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ up :
 
 down : 
 	@docker-compose -f ./srcs/docker-compose.yml down
+	@docker rmi $$(docker images -q)
+	@docker system prune -f ./srcs/docker-compose.yml
 
 stop : 
 	@docker-compose -f ./srcs/docker-compose.yml stop
--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Crea la directory per i certificati se non esiste
-mkdir -p /etc/ssl/certs
+# mkdir -p /etc/ssl/certs
 
 
 # Genera il certificato SSL
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/nginx-selfsigned.key \
-    -out /etc/ssl/certs/nginx-selfsigned.crt \
-    -subj "/C=IT/L=Roma/O=42Roma/OU=student/CN=grinella.42.fr"
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#     -keyout /etc/ssl/private/nginx-selfsigned.key \
+#     -out /etc/ssl/certs/nginx-selfsigned.crt \
+#     -subj "/C=IT/L=Roma/O=42Roma/OU=student/CN=grinella.42.fr"
 
 # Crea la configurazione di NGINX
 # cat > /etc/nginx/conf.d/default.conf <<EOF
